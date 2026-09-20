98fae5edf97c35157ed7982d5a4d679aa5ce8fd3
akb <info@islam.ms>
2026-09-20 15:08:49 +0200

    update modules/admin/utils/helpers.js

--- modules/admin/utils/helpers.js
+++ modules/admin/utils/helpers.js
@@ -18,42 +18,42 @@ export const getLangSettings = (lang, q) => {
 };
 
 export const constructQuery = (type, lang, q, ord, numberPerPage, queryOffset) => {
-    const hasOrd = !isNaN(parseInt(ord)) && ord;
-    const orderClause = hasOrd ? 'ord ASC' : 'date DESC';
+  const hasOrd = !isNaN(parseInt(ord)) && ord;
+  const orderClause = hasOrd ? 'ord ASC' : 'date DESC';
 
-    if (q) {
-      if (type === "post" && langList.includes(lang)) {
-        let base = `SELECT id, title, url, descr, ord, pub FROM post WHERE lang = $1 AND (title ILIKE $2 OR similarity(title, $2) > 0.1)`;
-        let paramsOrder = hasOrd ? ` AND ord >= $3` : '';
-        let limitOffset = hasOrd ? ` ORDER BY ${orderClause} LIMIT $4 OFFSET $5` : ` ORDER BY ${orderClause} LIMIT $3 OFFSET $4`;
-        return `${base}${paramsOrder}${limitOffset}`;
-      } else if (type === "nmbr") {
-        let base = `SELECT id, ord, nb, nbd, nbm, pub FROM mnumbers WHERE lang = $1 AND (nb ILIKE $2 OR nbd ILIKE $2 OR nbm ILIKE $2)`;
-        let paramsOrder = hasOrd ? ` AND ord >= $3` : '';
-        let limitOffset = hasOrd ? ` ORDER BY ${orderClause} LIMIT $4 OFFSET $5` : ` ORDER BY ${orderClause} LIMIT $3 OFFSET $4`;
-        return `${base}${paramsOrder}${limitOffset}`;
-      } else {
-        let base = `SELECT id, title, audio, ord, pub FROM messages WHERE (title ILIKE $1 OR similarity(title, $1) > 0.1) AND lang = $2 AND type = $3`;
-        let paramsOrder = hasOrd ? ` AND ord >= $4` : '';
-        let limitOffset = hasOrd ? ` ORDER BY ${orderClause} LIMIT $5 OFFSET $6` : ` ORDER BY ${orderClause} LIMIT $4 OFFSET $5`;
-        return `${base}${paramsOrder}${limitOffset}`;
-      }
+  if (q) {
+    if (type === "post" && langList.includes(lang)) {
+      let base = `SELECT id, title, url, descr, ord, pub FROM post WHERE lang = $1 AND (title ILIKE $2)`;
+      let paramsOrder = hasOrd ? ` AND ord >= $3` : '';
+      let limitOffset = hasOrd ? ` ORDER BY ${orderClause} LIMIT $4 OFFSET $5` : ` ORDER BY ${orderClause} LIMIT $3 OFFSET $4`;
+      return `${base}${paramsOrder}${limitOffset}`;
+    } else if (type === "nmbr") {
+      let base = `SELECT id, ord, nb, nbd, nbm, pub FROM mnumbers WHERE lang = $1 AND (nb ILIKE $2 OR nbd ILIKE $2 OR nbm ILIKE $2)`;
+      let paramsOrder = hasOrd ? ` AND ord >= $3` : '';
+      let limitOffset = hasOrd ? ` ORDER BY ${orderClause} LIMIT $4 OFFSET $5` : ` ORDER BY ${orderClause} LIMIT $3 OFFSET $4`;
+      return `${base}${paramsOrder}${limitOffset}`;
     } else {
-      if (type === "post" && langList.includes(lang)) {
-        let base = `SELECT id, title, url, descr, ord, pub FROM post WHERE lang = $1`;
-        let paramsOrder = hasOrd ? ` AND ord >= $2` : '';
-        let limitOffset = hasOrd ? ` ORDER BY ${orderClause} LIMIT $3 OFFSET $4` : ` ORDER BY ${orderClause} LIMIT $2 OFFSET $3`;
-        return `${base}${paramsOrder}${limitOffset}`;
-      } else if (type === "nmbr") {
-        let base = `SELECT id, ord, nb, nbd, nbm, pub FROM mnumbers WHERE lang = $1`;
-        let paramsOrder = hasOrd ? ` AND ord >= $2` : '';
-        let limitOffset = hasOrd ? ` ORDER BY ${orderClause} LIMIT $3 OFFSET $4` : ` ORDER BY ${orderClause} LIMIT $2 OFFSET $3`;
-        return `${base}${paramsOrder}${limitOffset}`;
-      } else {
-        let base = `SELECT id, title, audio, ord, pub FROM messages WHERE lang = $1 AND type = $2`;
-        let paramsOrder = hasOrd ? ` AND ord >= $3` : '';
-        let limitOffset = hasOrd ? ` ORDER BY ${orderClause} LIMIT $4 OFFSET $5` : ` ORDER BY ${orderClause} LIMIT $3 OFFSET $4`;
-        return `${base}${paramsOrder}${limitOffset}`;
-      }
+      let base = `SELECT id, title, audio, ord, pub FROM messages WHERE (title ILIKE $1) AND lang = $2 AND type = $3`;
+      let paramsOrder = hasOrd ? ` AND ord >= $4` : '';
+      let limitOffset = hasOrd ? ` ORDER BY ${orderClause} LIMIT $5 OFFSET $6` : ` ORDER BY ${orderClause} LIMIT $4 OFFSET $5`;
+      return `${base}${paramsOrder}${limitOffset}`;
     }
+  } else {
+    if (type === "post" && langList.includes(lang)) {
+      let base = `SELECT id, title, url, descr, ord, pub FROM post WHERE lang = $1`;
+      let paramsOrder = hasOrd ? ` AND ord >= $2` : '';
+      let limitOffset = hasOrd ? ` ORDER BY ${orderClause} LIMIT $3 OFFSET $4` : ` ORDER BY ${orderClause} LIMIT $2 OFFSET $3`;
+      return `${base}${paramsOrder}${limitOffset}`;
+    } else if (type === "nmbr") {
+      let base = `SELECT id, ord, nb, nbd, nbm, pub FROM mnumbers WHERE lang = $1`;
+      let paramsOrder = hasOrd ? ` AND ord >= $2` : '';
+      let limitOffset = hasOrd ? ` ORDER BY ${orderClause} LIMIT $3 OFFSET $4` : ` ORDER BY ${orderClause} LIMIT $2 OFFSET $3`;
+      return `${base}${paramsOrder}${limitOffset}`;
+    } else {
+      let base = `SELECT id, title, audio, ord, pub FROM messages WHERE lang = $1 AND type = $2`;
+      let paramsOrder = hasOrd ? ` AND ord >= $3` : '';
+      let limitOffset = hasOrd ? ` ORDER BY ${orderClause} LIMIT $4 OFFSET $5` : ` ORDER BY ${orderClause} LIMIT $3 OFFSET $4`;
+      return `${base}${paramsOrder}${limitOffset}`;
+    }
+  }
 };
